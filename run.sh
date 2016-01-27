e390dc134ff8fee7edf9dc22eca708ba808cebc3
Mark Fine <mark.fine@gmail.com>
2016-01-27 11:55:42 -0800

    rename to chan-perf.

--- run.sh
+++ run.sh
@@ -6,10 +6,10 @@ stack install --executable-profiling --library-profiling
 
 for n in 0 1 2 3 4 5
 do
-    skylark-perf "$n" +RTS -p -hy -s < "$DATA_FILE" > /dev/null
-    hp2ps -c skylark-perf.hp
-    cp skylark-perf.ps skylark-perf-"$n".ps
-    open skylark-perf-"$n".ps
+    chan-perf "$n" +RTS -p -hy -s < "$DATA_FILE" > /dev/null
+    hp2ps -c chan-perf.hp
+    cp chan-perf.ps chan-perf-"$n".ps
+    open chan-perf-"$n".ps
 done
 
 
